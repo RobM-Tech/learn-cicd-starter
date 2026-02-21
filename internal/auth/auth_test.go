c40e5bbc0e8ca520ae072aa0405c9ce8ab090385
RobM_Tech <robert.mathieu.tech@gmail.com>
2026-02-20 21:43:37 -0600

    check CI catches failures

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,50 @@
+package auth
+import (
+	"testing"
+	"net/http"
+)
+func TestGetAPIKey(t *testing.T) {
+	tests := []struct {
+		name			string
+		inputHeaders	http.Header
+		expectedKey		string
+		expectedError	error
+	}{
+		{
+			name: "Happy-path",
+			inputHeaders: http.Header{
+				"Authorization": []string{"ApiKey secret-123"},
+			},
+			expectedKey:   "secret-123",
+			expectedError: nil,
+		},
+		{
+			name: "No Key",
+			inputHeaders: http.Header{
+				"Authorization": []string{""},
+			},
+			expectedKey:   "secret-123",
+			expectedError: ErrNoAuthHeaderIncluded,
+		},
+		{
+			name: "No Header",
+			inputHeaders: http.Header{},
+			expectedKey:   "",
+			expectedError: ErrNoAuthHeaderIncluded,
+		},
+	}
+
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetAPIKey(tt.inputHeaders)
+			if (err != nil) != (tt.expectedError != nil) {
+				t.Errorf("error: %v, wantErr: %v", err, tt.expectedError)
+				return			
+			}
+			if err == nil && got != tt.expectedKey{
+				t.Errorf("GetAPIKey() = %v, expected: %v", got, tt.expectedKey)
+			}
+		})
+	}
+}
